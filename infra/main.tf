9147fded6d961a66583a069fe085bd0df14fccea
Zivan-R <135060460+Zivan-R@users.noreply.github.com>
2025-05-05 14:11:58 +0200

    Added main.tf and created bucket in GCP

---
+++ infra/main.tf
@@ -0,0 +1,20 @@
+terraform {
+    backend "gcs" { 
+        bucket = "hello-terraform-zbucket"
+        prefix = "hello-z" 
+    }
+}
+
+provider "google" {
+    project = var.project_id
+    region = var.region
+}
+
+resource "google_container_cluster" "primary" {
+    name = var.cluster
+    location = var.region
+    initial_node_count = 1
+    node_config { 
+        machine_type = "e2-medium" 
+    }
+}
