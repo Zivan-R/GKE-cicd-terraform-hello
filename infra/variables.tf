9bbd549c0abaa963c134867c2cd704361e1fead5
Zivan-R <135060460+Zivan-R@users.noreply.github.com>
2025-05-05 14:11:35 +0200

    Added variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+    type = string,
+    default = "projet-z1"
+}
+
+variable "region" {
+    type = string,
+    default = "europe-west9"
+}
+
+variable "cluster" {
+    type = string,
+    default = "hello-cluster-z"
+}
