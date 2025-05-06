d4fed33e906228dda533bf6f1daf6935c5716f84
Zivan-R <135060460+Zivan-R@users.noreply.github.com>
2025-05-06 09:49:26 +0200

    added monitoring

--- infra/main.tf
+++ infra/main.tf
@@ -14,8 +14,13 @@ resource "google_container_cluster" "primary" {
     name = var.cluster
     location = var.zone
     initial_node_count = 1
+
     node_config { 
-        machine_type = "e2-medium"
+        machine_type = "e2-small"
     }
+
+    logging_service = "logging.googleapis.com/kubernetes"
+    monitoring_service = "monitoring.googleapis.com/kubernetes"
+
     deletion_protection = false
 }
